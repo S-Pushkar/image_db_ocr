383be71e644436637c2a50de8d42195a3df6b90d
Pushkar S <spushkar0071234@gmail.com>
2024-09-14 18:20:15 +0530

    Added SentenceTransformer

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, File, UploadFile, BackgroundTasks
-from fastapi.responses import JSONResponse
 import easyocr
+from sentence_transformers import SentenceTransformer
+import numpy as np
 
 app = FastAPI()
 reader = easyocr.Reader(['en', 'kn'])
+# model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
 @app.get("/hello")
 def read_root():
@@ -12,9 +15,14 @@ def read_root():
 def read_text_from_image(image):
     result = reader.readtext(image, detail=0)
     print(result)
+    if len(result) > 0:
+        vectors = model.encode(result)
+        combined_vector = np.mean(vectors, axis=0)
+        print(vectors)
+        print(combined_vector)
 
 @app.post("/uploadfile/")
 async def upload_file(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
     image = file.file.read()
     background_tasks.add_task(read_text_from_image, image)
-    return JSONResponse(content={"message": "success"})
+    return {"message": "success"}
